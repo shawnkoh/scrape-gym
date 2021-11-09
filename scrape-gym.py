8a4229ceb5149217cf605a24ecab2c56ecb1a1b5
Shawn Koh <shawn@shawnkoh.sg>
2021-11-09 19:56:28 +0800

    Fin

--- scrape-gym.py
+++ scrape-gym.py
@@ -1,3 +1,19 @@
-from bs4 import BeautifulSoup
+import logging
 
-from requests
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+options = Options()
+options.add_argument("--headless")
+chrome_driver_manager = ChromeDriverManager(log_level=logging.CRITICAL).install()
+service = Service(chrome_driver_manager)
+driver = webdriver.Chrome(service=service, options=options)
+driver.get("https://smartentry.org/queue/book/e1tpy")
+
+XPATH = "/html/body/div[4]/div[1]/div/div[2]"
+queue = driver.find_element(by=By.XPATH, value=XPATH)
+
+print(queue.text)
